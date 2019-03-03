584708f043ac365876d399dfe2ffb62972dd875d
sergey.sedelnikov <sergey.sedelnikov@storaenso.com>
2019-03-03 21:26:03 +0200

    motor value is reported to the ui

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { initializeApiConnection } from './utils/api-connect'
+import { initializeApiConnection, onMotorSpeedReported } from './utils/api-connect'
 
 
 import './App.css';
@@ -8,24 +8,50 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      authResult: null
+      authResult: null,
+      motors: []
     }
   }
 
+  /**
+   * Handles the motor reported speed
+   * @param {*} motorId 
+   * @param {*} speed 
+   */
+  handleMotorSpeedReported(motorId, speed){
+    this.setState({
+      motors: [
+        {motorId, speed}
+      ]
+    })
+  }
+
+  /**
+   * Before component mount, connect to API, register callbacks.
+   */
   async componentWillMount(){
     const authResult = await initializeApiConnection();
     this.setState({
       authResult
     });
+
+    // register the callback to see when motor changes the speed
+    onMotorSpeedReported(this.handleMotorSpeedReported.bind(this));
   }
 
   render() {
-    const { authResult } = this.state;
+    const { authResult, motors } = this.state;
     return (
       <div className="container-fluid">
         <code>
           {authResult ? 'authenticated!' : 'not authenticated'}
         </code>
+
+        { motors.map(({motorId, speed}) => {
+          return <div key={motorId}>
+            {motorId} {speed}
+          </div>
+        }) }
       </div>
     );
   }
--- admin-dashboard/src/utils/api-connect.js
+++ admin-dashboard/src/utils/api-connect.js
@@ -4,6 +4,8 @@ import auth from '@feathersjs/authentication-client';
 import io from "socket.io-client";
 
 const apiUil = 'http://localhost:3030';
+// array of callbacks to call when motor reported the speed.
+const motorSpeedReportedCallbacks = [];
 
 /**
  * Initializes the connection to API server.
@@ -28,7 +30,26 @@ export async function initializeApiConnection() {
     app.configure(auth({
         storage: window.localStorage
     }));
+
+    // subscribe for the motor speed report method, 
+    // call all registered callbacks
+    app.service('api/v1/motor-speed').on('updated', (data) => {
+        // we expect the object with the properties
+        // id: "Motor1"
+        // speed: 34.68086776263067
+        // call all registered callbacks
+        motorSpeedReportedCallbacks.forEach(callback => callback(data.id, data.speed));
+    })
+
     // trying to authenticate with local storage JWT token
     const authResult = await app.authenticate();
     return authResult;
+}
+
+/**
+ * Registers the callback to execute when motor reported the speed.
+ * @param {*} callback Function callback to be called on motor reports it's speed.
+ */
+export function onMotorSpeedReported(callback){
+    motorSpeedReportedCallbacks.push(callback);
 }
